2935708dea85423e354b28f351689016b472cc50
flawlesscode254 <denzme414@gmail.com>
2021-11-24 18:19:38 +0300

    made the sidebar

--- pages/components/Sidebar.js
+++ pages/components/Sidebar.js
@@ -1,11 +1,20 @@
 import styled from 'styled-components'
 import Avatar from '@mui/material/Avatar';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 function Sidebar() {
     return (
         <Container>
             <Header>
-                <UserAvatar />
+                <Logo>
+                    <UserAvatar />
+                </Logo>
+                
+                <IconsContainer>
+                    <Chat />
+                    <Menu />
+                </IconsContainer>
             </Header>
         </Container>
     )
@@ -15,12 +24,34 @@ export default Sidebar
 
 
 const Container = styled.div`
-    
+
 `
 const Header = styled.div`
-    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    width: 15%;
 `
 
 const UserAvatar = styled(Avatar)`
     margin: 10px;
+    cursor: pointer;
+`
+
+const IconsContainer = styled.div`
+
+`
+
+const Chat = styled(ChatIcon)`
+    cursor: pointer;
+    margin-right: 15px;
+`
+
+const Menu = styled(MoreVertIcon)`
+    cursor: pointer;
+`
+
+const Logo = styled.div`
+    flex: 1px;
 `
